f63a6040fcffc6851d6ed8324d4fd9544a9d00f7
Gaurav Patel <Gaurav.Patel1@amdocs.com>
2021-08-22 14:37:54 +0530

    Adding programs

---
+++ src/com/java8/practice/programs/MoveZerosToEnd.java
@@ -0,0 +1,28 @@
+package com.java8.practice.programs;
+
+public class MoveZerosToEnd {
+
+    public static void main(String[] args) {
+        int nums[] = {1,0,3,0,0,7,8};
+        pushZerosToEnd(nums,nums.length);
+        for (int num : nums) {
+            System.out.print(num +",");
+        }
+    }
+
+    private static void pushZerosToEnd(int[] nums, int length) {
+        int count = 0;
+
+        // Traverse the array. If element encountered is
+        // non-zero, then replace the element at index 'count'
+        // with this element
+        for(int i=0 ;i<length;i++){
+            if(nums[i] !=0){
+                nums[count++] = nums[i];
+            }
+        }
+        while(count<length){
+            nums[count++] = 0;
+        }
+    }
+}
